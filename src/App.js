f0be001a4221a9e306f42c98d418ba1f6a147044
hari7587 <harish.h.blr87@gmail.com>
2023-03-20 13:35:56 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://media.giphy.com/media/ZErmptaZPGQQXeO2Lw/giphy-downsized-large.gif" />
+        <h1>I Love A Cup Of Tea...!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
